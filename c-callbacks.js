a717c72033c931e771c274b81daf1c6b932d177a
veroca88 <veronicadonoso1506@gmail.com>
2020-11-25 22:09:26 -0600

    Callback function practice

---
+++ c-callbacks.js
@@ -0,0 +1,22 @@
+// Callback function
+
+//Goal: Mess around with callback pattern
+//1. Define add function that accepts the corrects arguments.
+//2. Use setTimeOut to simulate 2 seconds delay.
+//3. After the 2 seconds are up. Call the callback function with the sum.
+//4. Test your work
+
+
+const add = (num1, num2, callback) => {
+    setTimeout(() => {
+        // console.log('Two seconds are up')
+        const sum = num1 + num2
+        callback(sum)
+        
+    }, 2000)
+    
+}
+
+add(1, 4, (sum) => {
+    console.log(sum)
+})
